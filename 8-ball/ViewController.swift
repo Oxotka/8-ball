cea9727bf861dc57e2bfbfb57cf49fc335b60900
Арипов Никита <aripovn@gmail.com>
2022-05-08 20:12:58 +0300

    modify code for app

--- 8-ball/ViewController.swift
+++ 8-ball/ViewController.swift
@@ -5,7 +5,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var predictionLabel: UILabel!
     @IBOutlet weak var ballImage: UIImageView!
     
-    let predictions = ["Да, конечно", "Нет!", "Возможно...", "Попробуй еще", "Лучше завтра", "Кажется, да", "Нельзя", "Сомнительно", "Бесспорно!", "Точно нет", "Ты знаешь ответ", "Подумай еще"]
+    let predictions = [
+        "Да, конечно", "Кажется, да", "Бесспорно!", "Давно пора", // Positive prediction
+        "Возможно...", "Сомнительно", "Ты уже знаешь ответ", "Решай сам", // Undefined prediction
+        "Нет!", "Думаю, не стоит", "Точно нет", "Лучше не надо", // Negative prediction
+        "Не знаю", "Попробуй еще", "Подумай еще", "Лучше завтра" // Try again
+    ]
+    
+    override func viewWillAppear(_ animated: Bool) {
+        predictionLabel.alpha = 0
+        UIView.animate(withDuration: 5, animations: {
+            self.predictionLabel.alpha = 0.7
+        })
+    }
     
     override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         UIView.animate(withDuration: 1, animations: {
@@ -15,19 +27,19 @@ class ViewController: UIViewController {
     }
     
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-        newPrediction()
+        showNewPrediction()
     }
     
-    @IBAction func newPrediction(_ sender: UIButton) {
-        newPrediction()
+    @IBAction func newPredictionTouch(_ sender: UIButton) {
+        showNewPrediction()
     }
     
-    func newPrediction(){
+    func showNewPrediction(){
         predictionLabel.alpha = 0
         UIView.animate(withDuration: 3, animations: {
             self.predictionLabel.alpha = 1
         })
         
-        predictionLabel.text = predictions[Int.random(in: 0...predictions.count-1)]
+        predictionLabel.text = predictions[Int.random(in: 0..<predictions.count)]
     }
 }
